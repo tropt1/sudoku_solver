7e31a641eeb8b38a205fbc66f2c06fbcc0400b5a
tropt1 <2717958@gmail.com>
2023-04-18 01:00:04 +0300

    aoaoaoa

--- sudoku.py
+++ sudoku.py
@@ -6,44 +6,38 @@ import solver
 
 pygame.init()
 
-# x = 385 y = 495 width = 90 heigth = 40
-class SolveButton:
-    def __init__(self, width, height, action):
+
+class Button:
+    def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.active_clr = (170, 180, 185)
+        self.inactive_clr = (130, 140, 150)
+        self.font = pygame.font.SysFont("Arial", 42)
 
-    def draw(self, x:int, y:int, message:str, action):
+    def draw(self, x: int, y: int, message: str, action=None):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
         if (x < mouse[0] < x + self.width) and (y < mouse[1] < y + self.height):
-            pygame.draw.rect(screen, (9, 130, 236), (x, y, self.width, self.height))
+            pygame.draw.rect(screen, self.active_clr, (x, y, self.width, self.height))
 
             if click[0] == 1:
-                ...
+                if action is not None:
+                    action()
 
         else:
-            pygame.draw.rect(screen, (9, 130, 236), (x, y, self.width, self.height))
+            pygame.draw.rect(screen, self.inactive_clr, (x, y, self.width, self.height))
 
-
-
-
-
-
-
-
-
-# arr = sudokum.generate(mask_rate=0.7)
-# for i in range(9):
-#     for j in range(9):
-#         if arr[i][j] == 0:
-#             arr[i][j] = " "
+        text_render = self.font.render(message, True, (255, 255, 255))
+        screen.blit(text_render, (x + 5, y - 5))
 
 
 WIDTH, HEIGHT = 500, 560
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 font1 = pygame.font.SysFont("Arial", 19)
 font2 = pygame.font.SysFont("Arial", 48)
+font3 = pygame.font.SysFont("Arial", 42)
 
 
 def draw_background():
@@ -82,15 +76,29 @@ def draw_numbers():
             n_text = font2.render(str(output), True, pygame.Color('black'))
             screen.blit(n_text, pygame.Vector2((col * 50) + offset - 1, (row * 50) + offset - 11))
 
+exit_button = Button(90, 45)
+generate_button = Button(175, 45)
+solve_button = Button(105, 45)
+
 
 def main():
     draw_background()
     write("sudoku", 12)
     draw_numbers()
+    exit_button.draw(395, 495, 'exit')
+    generate_button.draw(25, 495, 'generate')
+    solve_button.draw(210, 495, 'solve')
     pygame.display.flip()
-    solver.Sudoku(arr, 0, 0)
-    draw_numbers()
-    pygame.display.flip()
+
+
+
+    #solver.Sudoku(arr, 0, 0)
+    #draw_numbers()
+    #pygame.display.flip()
+
+
+
+
 
 
 generator()
